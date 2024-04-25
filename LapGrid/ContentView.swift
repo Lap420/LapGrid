459fd900a1ba0a20281cffdcd886ddeaec24c40b
Lap42 <laptevmiddleeast@gmail.com>
2024-04-25 14:40:46 +0400

    Added the third example with different elements height

--- LapGrid/ContentView.swift
+++ LapGrid/ContentView.swift
@@ -29,12 +29,29 @@ struct ContentView: View {
         TableElement(id: 4, title: "4", spans: 1),
         TableElement(id: 5, title: "5", spans: 1),
         TableElement(id: 6, title: "6 Spans Two", spans: 2),
+        TableElement(id: 7, title: "7 Spans Three", spans: 3),
+        TableElement(id: 8, title: "8", spans: 1),
+        TableElement(id: 9, title: "9", spans: 1),
+        TableElement(id: 10, title: "10 Spans Two", spans: 2),
+        TableElement(id: 11, title: "11", spans: 1),
+    ]
+    @State private var data3: [TableElement] = [
+        TableElement(id: 1, title: "1", spans: 1),
+        TableElement(id: 2, title: "2", spans: 1),
+        TableElement(id: 3, title: "3 Spans Two", spans: 2),
+        TableElement(id: 4, title: "4", spans: 1),
+        TableElement(id: 5, title: "5", spans: 1),
+        TableElement(id: 6, title: "6 Spans Two", spans: 2),
         TableElement(id: 7, title: "7", spans: 1),
         TableElement(id: 8, title: "8", spans: 1),
         TableElement(id: 9, title: "9", spans: 1),
         TableElement(id: 10, title: "10 Spans Two", spans: 2),
         TableElement(id: 11, title: "11", spans: 1),
     ]
+    
+    var randomHeight: CGFloat {
+        CGFloat.random(in: 30...80)
+        }
     
     var body: some View {
         ScrollView {
@@ -75,6 +92,28 @@ struct ContentView: View {
                         .onDrop(
                             of: [.text],
                             delegate: DragDropDelegate(draggedItem: $draggedItem, item: dataItem, data: $data2)
+                        )
+                    // Share span to the layout
+                        .span(dataItem.spans)
+                }
+            }
+            .padding(.bottom, 50)
+            
+            LapGrid(spacing: 12) {
+                ForEach(data3) { dataItem in
+                    Text(dataItem.title)
+                        .frame(maxWidth: .infinity, minHeight: randomHeight)
+                        .background(
+                            RoundedRectangle(cornerRadius: 8)
+                                .fill(Color.gray.secondary)
+                        )
+                        .onDrag {
+                            draggedItem = dataItem
+                            return NSItemProvider(object: "\(dataItem.id)" as NSString)
+                        }
+                        .onDrop(
+                            of: [.text],
+                            delegate: DragDropDelegate(draggedItem: $draggedItem, item: dataItem, data: $data3)
                         )
                     // Share span to the layout
                         .span(dataItem.spans)
