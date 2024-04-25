7ee18a948ae836c75c9cbe6717970d2c618e9f00
Lap42 <laptevmiddleeast@gmail.com>
2024-04-25 14:10:41 +0400

    Initial Commit

---
+++ LapGrid/LapGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  LapGridApp.swift
+//  LapGrid
+//
+//  Created by Sergei Laptev on 25/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct LapGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
